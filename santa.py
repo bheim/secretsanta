d287bf4fbfb1814a0d233d24415dd0996446791a
Benjamin Heim <benjaminheim@Benjamins-MacBook-Pro.local>
2023-12-12 12:11:17 -0500

    ready to add names, emails

--- santa.py
+++ santa.py
@@ -1,1 +1,32 @@
 #this is our secret santa file
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart 
+import smtplib
+import random
+import os
+
+
+smtp = smtplib.SMTP('smtp.gmail.com', 587) 
+smtp.ehlo() 
+smtp.starttls() 
+smtp.login('secretsantauchicago@gmail.com', 'cbyp rmzb gdno tomd')
+
+people = [("Ben", "benjaminheim43@gmail.com"), ("Grace", "gvs@uchicago.edu"),
+          ("Thomas", "thomasdegirolami@gmail.com")]
+chosen = []
+
+for j in range(len(people)):
+    i = j
+    while (i == j) or (i in chosen):
+        i = random.randint(0, len(people) - 1)
+    person = people[i][0]
+    text = f"Your secret santa person is {person}"
+    chosen.append(i)
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Secret Santa Person'
+    msg.attach(MIMEText(text))
+    to = [people[j][1]] 
+    smtp.sendmail(from_addr="secretsantauchicago@gmail.com", 
+              to_addrs=to, msg=msg.as_string())
+print("Emails sent!")
+smtp.quit()
