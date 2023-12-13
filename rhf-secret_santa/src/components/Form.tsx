502a3ad9fa2e5b1eb5ec2cf16d4c6c8a3e66b234
Grace Simmons <gvs@uchicago.edu>
2023-12-13 11:47:10 -0600

    added: logging information and errors

--- rhf-secret_santa/src/components/Form.tsx
+++ rhf-secret_santa/src/components/Form.tsx
@@ -1,19 +1,49 @@
-import React from "react";
+import { useForm } from 'react-hook-form';
+import { DevTool } from "@hookform/devtools";
+
+type FormValues = {
+    name: string
+    email: string
+}
 
 export const Form = () => {
-  return (
-    <div>
-        <form>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="email" name="name" />
+    const form = useForm<FormValues>();
+    const { register, control, handleSubmit } = form
 
-            <label htmlFor="emal">Email</label>
-            <input type="email" id="email" name="email" />
+    const onSubmit = (data: FormValues) => {
+        console.log('Form submitted', data)
+    }
+ 
+    return (
+        <div>
+            <h1> Form </h1>
+            <form onSubmit = {handleSubmit(onSubmit)} noValidate>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="email" {...register("name", { 
+                    required: {
+                        value: true,
+                        message: "email is required"
+                    }
+                })}/>
 
-            <button>Submit</button>
-        </form>
-    </div>
-  );
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" {...register("email", {
+                    required: {
+                        value: true,
+                        message: "email is required"
+                    },
+                    pattern: {
+                        value: 
+                            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                        message: "Invalid email format"
+                    }
+                })} />
+
+                <button>Submit</button>
+            </form>
+            <DevTool control = { control } />
+        </div>
+    );
 };
 
 
