4b2ba86350a2da757516ccd0b4dcbbc671296d2c
Grace Simmons <gvs@uchicago.edu>
2023-12-16 11:48:13 -0600

    added error messages to display

--- rhf-secret_santa/src/components/Form.tsx
+++ rhf-secret_santa/src/components/Form.tsx
@@ -8,7 +8,9 @@ type FormValues = {
 
 export const Form = () => {
     const form = useForm<FormValues>();
-    const { register, control, handleSubmit } = form
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
+
 
     const onSubmit = (data: FormValues) => {
         console.log('Form submitted', data)
@@ -18,14 +20,23 @@ export const Form = () => {
         <div>
             <h1> Form </h1>
             <form onSubmit = {handleSubmit(onSubmit)} noValidate>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="email" {...register("name", { 
-                    required: {
-                        value: true,
-                        message: "email is required"
-                    }
-                })}/>
+                <div className="form-control">
+                    <label htmlFor="name">Name</label>
+                    <input 
+                        type="text" 
+                        id="email" 
+                        {...register("name", { 
+                            required: {
+                                value: true,
+                                message: "name is required"
+                            }
+                        })}
+                    />
 
+                    <p className="error">{ errors.name?.message }</p>
+                </div>
+
+                <div className="form-control">
                 <label htmlFor="email">Email</label>
                 <input type="email" id="email" {...register("email", {
                     required: {
@@ -37,7 +48,11 @@ export const Form = () => {
                             /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
                         message: "Invalid email format"
                     }
-                })} />
+                })}
+            />
+            <p className="error">{ errors.email?.message }</p>
+            </div>
+
 
                 <button>Submit</button>
             </form>
